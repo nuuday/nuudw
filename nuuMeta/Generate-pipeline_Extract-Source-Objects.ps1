addaabc13245a0ad717d339b7be03fd350c48ad4
Mikkel Langelykke <milan@nuuday.dk>
2023-05-22 16:50:27 +0200

    Added powershell and templates

---
+++ nuuMeta/Generate-pipeline_Extract-Source-Objects.ps1
@@ -0,0 +1,137 @@
+﻿Param(
+    [Parameter(Position=0,Mandatory=$true)]
+    [string]$SourceConnectionIds
+)
+
+$ErrorActionPreference = "Stop"
+
+#Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+[Reflection.Assembly]::LoadWithPartialName("Newtonsoft.Json.dll")
+IMPORT-Module sqlserver
+
+# =====================
+# Set Variables
+# =====================
+
+# Set SQL where-clause depending on input
+$SQLWhereClause = "SourceConnectionId IN ($($SourceConnectionIds))"
+
+
+# Set paths
+Set-Location $($PSScriptRoot + "\")
+Set-Location ..
+$ReposPath = $(Get-Location).Path
+$nuuMetaPath = $ReposPath + '\nuuMeta\'
+$ADFPath = $ReposPath + '\ADF\'
+
+
+# The subscription ID for the Azure environment
+$AzureSubscriptionID = "155e9e90-807a-43a9-811b-8f7bdb95a801";
+
+# =====================
+# Login i Azure
+# =====================
+
+#Login-AzAccount -Tenant 'c95a25de-f20a-4216-bc84-99694442c1b5'
+#Select-AzSubscription $AzureSubscriptionID
+
+# The connectionstring for the Azure SQL DB
+$ConnectionString = Get-AzKeyVaultSecret -VaultName "nuudw-kv01-dev" -Name "ConnectionString-nuudwsqldb01" -AsPlainText
+$KeyVaultPrefix = Get-AzKeyVaultSecret -VaultName "nuudw-kv01-dev" -Name "KeyVaultPrefix"
+
+$SourceObjectDefinitions = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT * FROM nuuMetaView.SourceObjectDefinitions WHERE $($SQLWhereClause)" -MaxCharLength 12000
+$SourceConnectionDefinitions = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT [SourceConnectionName], [SourceObjectType], SourceConnectionType, 0 AS SourceIsFileObject FROM [nuuMetaView].[SourceConnectionDefinitions] WHERE $($SQLWhereClause)"
+$ADFDatabase = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT VariableValue AS DBName, 'AzureSqlTable' AS ObjectType FROM nuuMetaView.Variables WHERE VariableName = 'DatabaseNameMeta'"
+$NuuDLStagingLinkedService = (Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT VariableValue FROM nuuMetaView.Variables WHERE VariableName = 'NUUDLStagingLinkedService'").VariableValue
+
+$ExtractControllerDefinitions = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT * FROM nuuMetaView.ExtractControllerDefinitions WHERE $($SQLWhereClause)"
+$DistinctExtractControllerDefinitions = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT DISTINCT ADFControllerName, ADFFolder FROM [nuuMetaView].[ExtractControllerDefinitions] WHERE $($SQLWhereClause)"
+
+
+
+write-host ""
+write-host "============================="
+write-host "Create Extract Pipelines"
+write-host "============================="
+write-host ""
+
+foreach ($obj in $SourceObjectDefinitions) {
+
+    $ADFCopySourceType = $obj.ADFCopySourceType
+    $ADFFolderName = "0.1_Extracts"
+    
+
+    $ExtractTemplate = $obj.TemplateName   
+    $ExtractTemplate += ".json"  
+    
+    $template_pipeline = (Get-Content ($nuuMetaPath+'templates\'+$ExtractTemplate)) `
+        -replace "<%ADFFolderName%>",$ADFFolderName `
+        -replace "<%ADFPipelineName%>",$obj.ADFPipelineName `
+        -replace "<%ADFSourceQuery%>",$obj.SourceQuery `
+        -replace "<%ADFSourceIsReadyQuery%>",$obj.SourceIsReadyQuery `
+        -replace "<%ADFSourceConnection%>",$obj.SourceConnectionName `
+        -replace "<%ADFSourceConnectionType%>",$obj.SourceConnectionType `
+        -replace "<%ADFSourceDatasetName%>",$obj.SourceDatasetName `
+        -replace "<%ADFSourceSchema%>",$obj.SourceSchemaName `
+        -replace "<%ADFSourceTable%>",$obj.SourceObjectName `
+        -replace "<%ADFSourceFileSystem%>",$obj.FileSystem `
+        -replace "<%ADFSourceFileFolder%>",$obj.Folder `
+        -replace "<%ADFSourceFileName%>",$obj.FullFileName `
+        -replace "<%ADFDestinationLinkedService%>",$ADFDatabase.DBName `
+        -replace "<%ADFDestinationSchema%>",$obj.DestinationSchemaName `
+        -replace "<%ADFDestinationTable%>",$obj.DestinationTableName `
+        -replace "<%ADFDestinationDatasetName%>", $($ADFDatabase.DBName + "_DynamicDataset") `
+        -replace "<%ADFHistoryTrackingColumns%>",$obj.HistoryTrackingColumns `
+        -replace "<%ADFCopySourceType%>",$obj.ADFCopySourceType`
+        -replace "<%ADFWatermarkColumnName%>",$obj.WatermarkColumnName `
+        -replace "<%ADFWatermarkIsDate%>",$obj.WatermarkIsDate `
+        -replace "<%ADFWatermarkInQuery%>",$obj.WatermarkInQuery `
+        -replace "<%ADFWatermarkRollingWindowDays%>",$obj.WatermarkRollingWindowDays `
+        -replace "<%ADFRollingWindowDays%>",$obj.WatermarkRollingWindowDays `
+        -replace "<%ADFPartitionSQLScript%>",$obj.PartitionSQLScript `
+        -replace "<%ADFSqlType%>",$obj.AzureSQLType `
+        -replace "<%KeyVaultPrefix%>",$KeyVaultPrefix.SecretValueText `
+        -replace "<%VariableSchemaNameSecret%>",$obj.SchemaName `
+        -replace "<%ADFNuuDLStagingLinkedService%>", $NuuDLStagingLinkedService `
+        
+        Write-Host "Generating pipeline" $obj.ADFPipelineName"...." -ForegroundColor Yellow  -NoNewline
+        
+        Set-Content -Path ($ADFPath+"pipeline\" + $obj.ADFPipelineName` + ".json") -Value ([Newtonsoft.Json.Linq.JObject]::Parse($template_pipeline).ToString()) -NoNewline
+
+        Write-Host " Done"-ForegroundColor Green    
+
+}
+
+write-host ""
+write-host "============================="
+write-host "Create Extract Controller Pipelines"
+write-host "============================="
+write-host ""
+
+
+foreach ($ctrl in $DistinctExtractControllerDefinitions) {
+
+    $ControllerCode = (Get-Content ($nuuMetaPath+'templates\Template_Pipeline_Controller_Extract.json') ) `
+        -replace "<%ADFControllerName%>",$ctrl.ADFControllerName `
+        -replace "<%ADFControllerFolder%>",$ctrl.ADFFolder `   
+     
+
+    $Controller = ($ControllerCode | ConvertFrom-Json)
+
+    ($Controller.properties) | add-member -MemberType NoteProperty -Name "activities" -Value @() 
+
+    foreach ($pack in $ExtractControllerDefinitions) {
+        $ActivityName = $pack.ADFPipelineActivityName.SubString(0,[math]::min(55,$pack.ADFPipelineActivityName.length) )
+        $Package = ('{"name": "' + $ActivityName + '","type": "ExecutePipeline","typeProperties":{"pipeline": {"referenceName": "' + $pack.ADFPipelineName + '","type": "PipelineReference"},"waitOnCompletion": true,"parameters": { "JobIsIncremental": "@pipeline().parameters.JobIsIncremental", "WriteBatchSize": "@pipeline().parameters.WriteBatchSize" }}}' | ConvertFrom-Json)
+        $Controller.properties.activities += $Package
+        $ActivityName = $null
+    }
+
+    Write-Host "Generating pipline" $ctrl.ADFControllerName"...." -ForegroundColor Yellow  -NoNewline
+
+    Set-Content -Path ($ADFPath+"pipeline\" + $ctrl.ADFControllerName  + ".json") -Value ([Newtonsoft.Json.Linq.JObject]::Parse(( $Controller | ConvertTo-Json -Depth 50)).ToString()) -NoNewline
+
+    Write-Host " Done"-ForegroundColor Green  
+
+  }
---
+++ nuuMeta/Generate-pipeline_Transform-and-Load-DW-Objects.ps1
@@ -0,0 +1,169 @@
+﻿Param(
+    [Parameter(Position=0,Mandatory=$true)]
+    [string]$DWObjectIds
+)
+
+$ErrorActionPreference = "Stop"
+
+#Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+[Reflection.Assembly]::LoadWithPartialName("Newtonsoft.Json.dll")
+IMPORT-Module sqlserver
+
+# =====================
+# Set Variables
+# =====================
+
+
+# Set SQL where-clause depending on input
+if ($DWObjectIds -eq "*") {
+    $SQLWhereClause = ""
+} else {
+    $SQLWhereClause = "AND DWObjectID IN ($DWObjectIds)"
+}
+
+
+# Set paths
+Set-Location $($PSScriptRoot + "\")
+Set-Location ..
+$ReposPath = $(Get-Location).Path
+$nuuMetaPath = $ReposPath + '\nuuMeta\'
+$ADFPath = $ReposPath + '\ADF\'
+
+# The subscription ID for the Azure environment
+$AzureSubscriptionID = "155e9e90-807a-43a9-811b-8f7bdb95a801";
+
+# =====================
+# Login i Azure
+# =====================
+
+#Login-AzAccount -Tenant 'c95a25de-f20a-4216-bc84-99694442c1b5'
+Select-AzSubscription $AzureSubscriptionID
+
+# The connectionstring for the Azure SQL DB
+$ConnectionString = Get-AzKeyVaultSecret -VaultName "nuudw-kv01-dev" -Name "ConnectionString-nuudwsqldb01" -AsPlainText
+
+
+# =====================
+# Datasets
+# =====================
+
+$DWObjectDefinitions = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT * FROM nuuMetaView.DWObjectDefinitions WHERE DWObjectName NOT IN ('Calendar','Time') $SQLWhereClause"
+$ADFDatabase = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "SELECT VariableValue AS DBName FROM nuuMetaView.Variables WHERE VariableName = 'DatabaseNameMeta'"
+
+# =====================
+# Create Load and Transform pipelines
+# =====================
+
+Invoke-Sqlcmd -ConnectionString $ConnectionString -Query "EXECUTE [nuuMeta].[CreateTransformProcedures]"
+
+write-host ""
+write-host "# ============================="
+write-host "# Create Load and Transform Pipelines"
+write-host "# ============================="
+write-host ""
+
+foreach ($rec in $DWObjectDefinitions) { 
+    
+    #Transform
+
+    $TransformPatternName = $nuuMetaPath+'Templates\Template_Pipeline_Transform.json'
+    
+    $Transform_Pipeline = (Get-Content ($TransformPatternName)) `
+       -replace "<%ADFLinkedServiceName%>",$ADFDatabase.DBName `
+       -replace "<%ADFDWObjectName%>",$rec.DWObjectName `
+       -replace "<%ADFTransformProcedureName%>",$rec.TransformProcedureName `
+       -replace "<%ADFStageTableName%>",$rec.StageTableName `
+       -replace "<%ADFPipelineFolder%>",$rec.PipelineFolder `
+       -replace "<%ADFTransformPipelineName%>",$rec.TransformPipelineName `
+       -replace "<%ADFLoadPipelineName%>",$rec.LoadPipelineName `
+
+    Write-Host "Generating pipline $($rec.TransformPipelineName)..." -ForegroundColor Yellow -NoNewline
+          
+    Set-Content -Path ($ADFPath+"pipeline\" + $rec.TransformPipelineName + ".json") -Value ([Newtonsoft.Json.Linq.JObject]::Parse($Transform_Pipeline).ToString()) -NoNewline
+
+    Write-Host " Done"-ForegroundColor Green  
+
+
+
+    #Load
+    
+    $DimensionPatternName = $null
+    $FactPatternName = $null
+
+    if ($rec.DWObjectType -eq 'Dimension') 
+    { 
+        $DimensionPatternName = 'Templates\Template_Pipeline_Load_Dimension.json'
+        
+    }
+    elseif ($rec.DWObjectType -in 'Fact','Bridge')
+    {
+        $FactPatternName = 'Templates\Template_Pipeline_Load_Fact.json'        
+    }  
+
+    #Dimensions
+    if ($DimensionPatternName -ne $null) {
+        $Dimension_Pipeline = (Get-Content ($nuuMetaPath+$DimensionPatternName)) `
+           -replace "<%ADFLinkedServiceName%>",$ADFDatabase.DBName `
+           -replace "<%ADFStageTableName%>",$rec.StageTableName `
+           -replace "<%ADFDWTableName%>",$rec.DWTableName `
+           -replace "<%ADFPipelineFolder%>",$rec.PipelineFolder `
+           -replace "<%ADFLoadPipelineName%>",$rec.LoadPipelineName `
+           -replace "<%ADFLoadProcedure%>",$rec.LoadProcedure `
+    
+         Write-Host "Generating pipline $($rec.LoadPipelineName)..." -ForegroundColor Yellow -NoNewline
+
+         Set-Content -Path ($ADFPath+"pipeline\" + $rec.LoadPipelineName + ".json") -Value ([Newtonsoft.Json.Linq.JObject]::Parse($Dimension_Pipeline).ToString()) -NoNewline
+
+         Write-Host " Done"-ForegroundColor Green 
+   
+     }    
+
+
+    #Facts and Bridges    
+    if ($FactPatternName -ne $null) {
+        $Fact_Pipeline = (Get-Content ($nuuMetaPath+$FactPatternName)) `
+           -replace "<%ADFLinkedServiceName%>",$ADFDatabase.DBName `
+           -replace "<%ADFDWSchemaName%>",$rec.DWSchemaName `
+           -replace "<%ADFDWTableName%>",$rec.DWTableName `
+           -replace "<%ADFStageSchemaName%>",$rec.StageSchemaName `
+           -replace "<%ADFStageTableName%>",$rec.StageTableName `
+           -replace "<%ADFPipelineFolder%>",$rec.PipelineFolder `
+           -replace "<%ADFLoadPattern%>",$rec.LoadPattern `
+           -replace "<%ADFLoadPipelineName%>",$rec.LoadPipelineName `
+           -replace "<%ADFLoadProcedure%>",$rec.LoadProcedure `
+
+         Write-Host "Generating pipline $($rec.LoadPipelineName)...." -ForegroundColor Yellow  -NoNewline
+
+         Set-Content -Path ($ADFPath+"pipeline\" + $rec.LoadPipelineName + ".json") -Value ([Newtonsoft.Json.Linq.JObject]::Parse($Fact_Pipeline).ToString()) -NoNewline
+
+         Write-Host " Done"-ForegroundColor Green    
+     }
+
+
+    
+    #Controller    
+    if ($rec.DWObjectType -eq 'Link') {
+        $ControllerPatternName = $nuuMetaPath+'Templates\Template_Pipeline_Controller_DW_Exclude_Load.json'
+    } else {
+        $ControllerPatternName = $nuuMetaPath+'Templates\Template_Pipeline_Controller_DW.json'
+    }
+
+    $Controller_Pipeline = (Get-Content ($ControllerPatternName)) `
+        -replace "<%ADFLinkedServiceName%>",$ADFDatabase.DBName `
+        -replace "<%ADFStageTableName%>",$rec.StageTableName `
+        -replace "<%ADFDWTableNamee%>",$rec.DWTableName `
+        -replace "<%ADFPipelineFolder%>",$rec.PipelineFolder `
+        -replace "<%ADFTransformPipelineName%>",$rec.TransformPipelineName `
+        -replace "<%ADFLoadPipelineName%>",$rec.LoadPipelineName `
+        -replace "<%ADFControllerPipelineName%>",$rec.ControllerPipelineName `
+
+    Write-Host "Generating pipline $($rec.ControllerPipelineName)...." -ForegroundColor Yellow  -NoNewline
+
+    Set-Content -Path ($ADFPath+"pipeline\" + $rec.ControllerPipelineName + ".json") -Value ([Newtonsoft.Json.Linq.JObject]::Parse($Controller_Pipeline).ToString()) -NoNewline
+
+    Write-Host " Done"-ForegroundColor Green    
+  
+
+}
+
