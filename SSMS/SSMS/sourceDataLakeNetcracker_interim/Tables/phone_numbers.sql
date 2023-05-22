48ce8ece266e70da8c42196629de3c46943619a2
Michael Gamborg Hansen <migh@kapacity.dk>
2023-05-22 11:01:25 +0200

    SourcerDataLakeNetCracker Schema and Tables have been created on NuudW

---
+++ SSMS/SSMS/sourceDataLakeNetcracker_interim/Tables/phone_numbers.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE [sourceDataLakeNetcracker_interim].[phone_numbers] (
+    [phone_number]                BIGINT         NULL,
+    [description]                 NVARCHAR (500) NULL,
+    [status]                      NVARCHAR (500) NULL,
+    [category]                    NVARCHAR (500) NULL,
+    [perform_auto_categorization] BIT            NULL,
+    [serving_switch_id]           NVARCHAR (500) NULL,
+    [top_range_id]                NVARCHAR (500) NULL,
+    [subrange_id]                 NVARCHAR (500) NULL,
+    [reservation_id]              NVARCHAR (500) NULL,
+    [customer_account_id]         NVARCHAR (500) NULL,
+    [ported_in]                   BIT            NULL,
+    [ported_out]                  BIT            NULL,
+    [metadata]                    NVARCHAR (500) NULL,
+    [aging_period_end_date]       NVARCHAR (500) NULL,
+    [status_change_date]          NVARCHAR (500) NULL,
+    [created_at]                  NVARCHAR (500) NULL,
+    [modified_at]                 NVARCHAR (500) NULL,
+    [first_owner_id]              NVARCHAR (500) NULL,
+    [ported_in_from]              NVARCHAR (500) NULL,
+    [ported_out_to]               NVARCHAR (500) NULL,
+    [country_code]                NVARCHAR (500) NULL,
+    [area_code]                   NVARCHAR (500) NULL,
+    [national_prefix]             NVARCHAR (500) NULL,
+    [is_deleted]                  BIT            NULL,
+    [last_modified_ts]            NVARCHAR (500) NULL,
+    [DWCreatedDate]               DATETIME       DEFAULT (getdate()) NULL
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'TruncateBeforeDeploy', @value = N'True', @level0type = N'SCHEMA', @level0name = N'sourceDataLakeNetcracker_interim', @level1type = N'TABLE', @level1name = N'phone_numbers';
+
---
+++ SSMS/SSMS/sourceDataLakeNetcracker_interim/Tables/product_instance.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [sourceDataLakeNetcracker_interim].[product_instance] (
+    [id]                   NVARCHAR (500) NULL,
+    [active_from]          NVARCHAR (500) NULL,
+    [active_to]            NVARCHAR (500) NULL,
+    [version]              BIGINT         NULL,
+    [schema_version]       NVARCHAR (500) NULL,
+    [state]                NVARCHAR (500) NULL,
+    [offering_id]          NVARCHAR (500) NULL,
+    [customer_id]          NVARCHAR (500) NULL,
+    [customer_category_id] NVARCHAR (500) NULL,
+    [root_id]              NVARCHAR (500) NULL,
+    [accountref_id]        NVARCHAR (500) NULL,
+    [start_date]           NVARCHAR (500) NULL,
+    [termination_date]     NVARCHAR (500) NULL,
+    [DWCreatedDate]        DATETIME       DEFAULT (getdate()) NULL
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'TruncateBeforeDeploy', @value = N'True', @level0type = N'SCHEMA', @level0name = N'sourceDataLakeNetcracker_interim', @level1type = N'TABLE', @level1name = N'product_instance';
+
