7efccf36b24d4f8736aede53cc724b7ea9eddbb6
Dennis Randeris Sørensen <deso@kapacity.dk>
2023-05-31 15:13:16 +0200

    Churn and Active subscriptions

--- SSMS/SSMS/stage/Stored Procedures/Transform_ProductTransactions.sql
+++ SSMS/SSMS/stage/Stored Procedures/Transform_ProductTransactions.sql
@@ -32,11 +32,11 @@ TRUNCATE TABLE [stage].[ProductTransactions]
 
 	SELECT 
 	CONVERT(NVARCHAR(50), pin.id)								AS ProductTransactionsIdentifier
-	,CONVERT(DATE, pin.active_from)								AS CalendarKey
+	,CONVERT(DATE, REPLACE(pin.start_date, '"', ''))			AS CalendarKey
 	,CONVERT(NVARCHAR(50), REPLACE(pin.offering_id, '"', ''))	AS ProductKey
 	,CONVERT(NVARCHAR(50), REPLACE(pin.customer_id, '"', ''))	AS CustomerKey
-	,1															AS ProductTransactionsQuantity
-	,CONVERT(NVARCHAR(10), 'Gross Add')							AS ProductTransactionsType
+	,1															AS ProductTransactionsQuantity /* Used for calculating the active subscriptions */
+	,CONVERT(NVARCHAR(10), 'Gross Add')							AS ProductTransactionsType /*This query only gets the Gross Add due to the specification in the where clause */
 	,GETDATE()													AS DWCreatedDate
 	--INTO stage.ProductTransactions
 	FROM [sourceDataLakeNetcracker_interim].[product_instance] pin 
@@ -54,3 +54,30 @@ TRUNCATE TABLE [stage].[ProductTransactions]
 	*/
 
 	WHERE pin.state = 'ACTIVE' --Business activation 
+
+	UNION ALL
+
+	SELECT 
+	CONVERT(NVARCHAR(50), pin.id)								AS ProductTransactionsIdentifier
+	,CONVERT(DATE, REPLACE(pin.termination_date, '"', ''))		AS CalendarKey
+	,CONVERT(NVARCHAR(50), REPLACE(pin.offering_id, '"', ''))	AS ProductKey
+	,CONVERT(NVARCHAR(50), REPLACE(pin.customer_id, '"', ''))	AS CustomerKey
+	,-1															AS ProductTransactionsQuantity /* Used for calculating the active subscriptions */
+	,CONVERT(NVARCHAR(10), 'Churn')								AS ProductTransactionsType /*This query only gets the Churn due to the specification in the where clause */
+	,GETDATE()													AS DWCreatedDate
+	--INTO stage.ProductTransactions
+	FROM [sourceDataLakeNetcracker_interim].[product_instance] pin 
+
+	/* We need to be able to link a specific product_instace transaction to a specific phone_number in order to determine
+	   where the phone_number was ported to so we can decide if the transaction (Churn) is external or internal.
+	   Question in that regard forwarded to Netcracker, but no answer recieved yet
+
+	--LEFT JOIN [sourceDataLakeNetcracker_interim].[phone_numbers] pn ON
+	--	pn.customer_account_id = REPLACE(pin.customer_id, '"', '') 
+
+	Joining on customer_id as above is not sufficient as a customer potentially can have several numbers.
+	TODO: Investigate if the business termination dates in phone_numbers matches the termination dates in product_instance
+
+	*/
+
+	WHERE pin.state = 'DISCONNECTED' --DISCONNECTED = Churn 
