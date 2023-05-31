45ad72e21ce3e4a2270ce6c5d60671ffe2b19a50
Mikkel Langelykke <milan@nuuday.dk>
2023-05-31 20:35:20 +0200

    Updated facts tables

--- SSMS/SSMS/fact/Tables/ProductTransactions_Temp.sql
+++ SSMS/SSMS/fact/Tables/ProductTransactions_Temp.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [fact].[ProductTransactions] (
+﻿CREATE TABLE [fact].[ProductTransactions_Temp] (
     [ProductTransactionsIdentifier] NVARCHAR (50) NULL,
     [CalendarID]                    INT           DEFAULT ((-1)) NOT NULL,
     [ProductID]                     INT           DEFAULT ((-1)) NOT NULL,
@@ -8,11 +8,4 @@
     [DWCreatedDate]                 DATETIME2 (0) NOT NULL,
     [DWModifiedDate]                DATETIME2 (0) NOT NULL
 );
-
-
-
-
-GO
-CREATE CLUSTERED COLUMNSTORE INDEX [CCI_ProductTransactions]
-    ON [fact].[ProductTransactions];
 
