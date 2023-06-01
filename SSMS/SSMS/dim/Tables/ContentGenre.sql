35d671e4e4b82a22fc1d940f194db37a3341471b
Louise Kragh Nielsen <louin@nuuday.dk>
2023-06-01 07:57:24 +0200

    added Content ssms obj

---
+++ SSMS/SSMS/dim/Tables/ContentGenre.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dim].[ContentGenre] (
+    [ContentGenreID]  INT             IDENTITY (1, 1) NOT NULL,
+    [ContentGenreKey] NVARCHAR (1024) NULL,
+    [DWIsCurrent]     BIT             NOT NULL,
+    [DWValidFromDate] DATETIME2 (0)   NOT NULL,
+    [DWValidToDate]   DATETIME2 (0)   NOT NULL,
+    [DWCreatedDate]   DATETIME2 (0)   NOT NULL,
+    [DWModifiedDate]  DATETIME2 (0)   NOT NULL,
+    [DWIsDeleted]     BIT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([ContentGenreID] ASC),
+    CONSTRAINT [NCI_ContentGenre] UNIQUE NONCLUSTERED ([ContentGenreKey] ASC, [DWValidFromDate] ASC)
+);
+
---
+++ SSMS/SSMS/dimView/Views/Content Genre.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dimView].[Content Genre] 
+AS
+SELECT
+	[ContentGenreID]
+	,[ContentGenreKey] AS [Content Genre Key]
+	,[DWIsCurrent]
+	,[DWValidFromDate]
+	,[DWValidToDate]
+	,[DWCreatedDate]
+	,[DWModifiedDate]
+	,[DWIsDeleted]
+	
+FROM [dim].[ContentGenre]
