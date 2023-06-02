206bef0391e2fdf5d002b22c73e49774df47d5fb
Mikkel Langelykke <milan@nuuday.dk>
2023-06-02 09:51:56 +0200

    Updated structure

--- SSMS/SSMS/dim/Tables/Product.sql
+++ SSMS/SSMS/dim/Tables/Product.sql
@@ -4,14 +4,16 @@
     [ProductName]     NVARCHAR (250) NULL,
     [ProductType]     NVARCHAR (50)  NULL,
     [DWIsCurrent]     BIT            NOT NULL,
-    [DWValidFromDate] DATETIME       NOT NULL,
-    [DWValidToDate]   DATETIME       NOT NULL,
-    [DWCreatedDate]   DATETIME       NOT NULL,
-    [DWModifiedDate]  DATETIME       NOT NULL,
+    [DWValidFromDate] DATETIME2 (0)  NOT NULL,
+    [DWValidToDate]   DATETIME2 (0)  NOT NULL,
+    [DWCreatedDate]   DATETIME2 (0)  NOT NULL,
+    [DWModifiedDate]  DATETIME2 (0)  NOT NULL,
     [DWIsDeleted]     BIT            NOT NULL,
     PRIMARY KEY CLUSTERED ([ProductID] ASC),
     CONSTRAINT [NCI_Product] UNIQUE NONCLUSTERED ([ProductKey] ASC, [DWValidFromDate] ASC)
 );
+
+
 
 
 
--- SSMS/SSMS/dimView/Views/Customer.sql
+++ SSMS/SSMS/dimView/Views/Customer.sql
@@ -1,10 +1,17 @@
-﻿CREATE VIEW [dimView].[Customer] AS
+﻿CREATE VIEW [dimView].[Customer] 
+AS
 SELECT
- [CustomerID]
-,[CustomerKey] AS [Customer Key]
-,[CustomerNo] AS [Customer No]
-,[CustomerName] AS [Customer Name]
-,[CustomerSegment] AS [Customer Segment]
-,[CustomerStatus] AS [Customer Status]
-
-  FROM [dim].[Customer]
+	[CustomerID]
+	,[CustomerKey] AS [Customer Key]
+	,[CustomerNo] AS [Customer No]
+	,[CustomerName] AS [Customer Name]
+	,[CustomerSegment] AS [Customer Segment]
+	,[CustomerStatus] AS [Customer Status]
+	,[DWIsCurrent]
+	,[DWValidFromDate]
+	,[DWValidToDate]
+	,[DWCreatedDate]
+	,[DWModifiedDate]
+	,[DWIsDeleted]
+	
+FROM [dim].[Customer]
