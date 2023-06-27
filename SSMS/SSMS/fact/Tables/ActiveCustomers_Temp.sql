9351dbfb7e6fc24646e58c194c9fef302eeb281c
Mikkel Langelykke <milan@nuuday.dk>
2023-06-27 21:53:45 +0200

    Added ActiveCustomers

---
+++ SSMS/SSMS/fact/Tables/ActiveCustomers_Temp.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [fact].[ActiveCustomers_Temp] (
+    [CalendarID]               INT           DEFAULT ((-1)) NOT NULL,
+    [ActiveCustomersCountDate] DATE          NULL,
+    [ActiveCustomersCount]     INT           NULL,
+    [DWCreatedDate]            DATETIME2 (0) NOT NULL,
+    [DWModifiedDate]           DATETIME2 (0) NOT NULL
+);
+
---
+++ SSMS/SSMS/stage/Tables/Fact_ActiveCustomers.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [stage].[Fact_ActiveCustomers] (
+    [CalendarKey]              DATE NULL,
+    [ActiveCustomersCountDate] DATE NULL,
+    [ActiveCustomersCount]     INT  NULL
+);
+
