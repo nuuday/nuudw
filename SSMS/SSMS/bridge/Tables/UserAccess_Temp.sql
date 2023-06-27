dfadba103cf37ac46a38fd7a2b20a4be29ba3da6
Mikkel Langelykke <milan@nuuday.dk>
2023-06-27 21:02:52 +0200

    Added UserAccess

---
+++ SSMS/SSMS/bridge/Tables/UserAccess_Temp.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [bridge].[UserAccess_Temp] (
+    [Legacy_EmployeeID] INT            DEFAULT ((-1)) NOT NULL,
+    [TopManager]        INT            NULL,
+    [EmployeeName]      NVARCHAR (255) NULL,
+    [UserName]          NVARCHAR (255) NULL,
+    [DWCreatedDate]     DATETIME2 (0)  NOT NULL,
+    [DWModifiedDate]    DATETIME2 (0)  NOT NULL
+);
+
---
+++ SSMS/SSMS/stage/Tables/Bridge_UserAccess.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [stage].[Bridge_UserAccess] (
+    [Legacy_EmployeeKey] NVARCHAR (255) NULL,
+    [TopManager]         INT            NULL,
+    [EmployeeName]       NVARCHAR (255) NULL,
+    [UserName]           NVARCHAR (255) NULL
+);
+
