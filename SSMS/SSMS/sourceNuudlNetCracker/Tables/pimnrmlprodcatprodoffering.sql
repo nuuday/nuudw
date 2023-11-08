d97636d2f86c65e52f0dbd96c7f2b35b54baaeed
Mohamed Jalali <moja@nuuday.dk>
2023-11-08 15:17:58 +0100

    c

---
+++ SSMS/SSMS/sourceNuudlNetCracker/Tables/pimnrmlprodcatprodoffering.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [sourceNuudlNetCracker].[pimnrmlprodcatprodoffering] (
+    [product_catalog_id]   NVARCHAR (36) NULL,
+    [product_offering_id]  NVARCHAR (36) NULL,
+    [cdc_revision_id]      NVARCHAR (36) NULL,
+    [NUUDL_ValidFrom]      DATETIME2 (7) NULL,
+    [NUUDL_ValidTo]        DATETIME2 (7) NULL,
+    [NUUDL_IsCurrent]      BIT           NULL,
+    [NUUDL_ID]             BIGINT        NOT NULL,
+    [NUUDL_CuratedBatchID] BIGINT        NULL,
+    [DWCreatedDate]        DATETIME2 (7) DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_pimnrmlprodcatprodoffering] PRIMARY KEY NONCLUSTERED ([NUUDL_ID] ASC)
+);
+
+
+GO
+CREATE CLUSTERED COLUMNSTORE INDEX [CCI_pimnrmlprodcatprodoffering]
+    ON [sourceNuudlNetCracker].[pimnrmlprodcatprodoffering];
+
---
+++ SSMS/SSMS/sourceNuudlNetCrackerView/Views/pimnrmlprodcatprodoffering_History.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE VIEW [sourceNuudlNetCrackerView].[pimnrmlprodcatprodoffering_History]
+AS
+SELECT 
+	[product_catalog_id] ,
+	[product_offering_id] ,
+	[cdc_revision_id] ,
+	[NUUDL_ValidFrom] ,
+	[NUUDL_ValidTo] ,
+	[NUUDL_IsCurrent] ,
+	[NUUDL_ID] ,
+	[NUUDL_CuratedBatchID] 
+	,[DWIsCurrent]
+	,[DWValidFromDate]
+	,[DWValidToDate]
+	,[DWCreatedDate]
+	,[DWModifiedDate]
+	,[DWIsDeletedInSource]
+	,[DWDeletedInSourceDate]
+FROM [sourceNuudlNetCracker].[pimnrmlprodcatprodoffering_History]
+WHERE DWIsCurrent = 1
