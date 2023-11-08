12226ad4a589e88d48ab7329746c010acb1ec292
Mohamed Jalali <moja@nuuday.dk>
2023-11-08 18:45:00 +0100

    c

---
+++ SSMS/SSMS/sourceNuudlNetCracker/Tables/nrmcusthasproductkeyname_History.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [sourceNuudlNetCracker].[nrmcusthasproductkeyname_History] (
+    [customer_ref]          NVARCHAR (300) NULL,
+    [product_seq]           INT            NULL,
+    [name]                  NVARCHAR (300) NULL,
+    [is_current]            BIT            NULL,
+    [NUUDL_ValidFrom]       DATETIME2 (7)  NULL,
+    [NUUDL_ValidTo]         DATETIME2 (7)  NULL,
+    [NUUDL_IsCurrent]       BIT            NULL,
+    [NUUDL_ID]              BIGINT         NOT NULL,
+    [NUUDL_CuratedBatchID]  BIGINT         NULL,
+    [DWIsCurrent]           BIT            NULL,
+    [DWValidFromDate]       DATETIME2 (7)  NOT NULL,
+    [DWValidToDate]         DATETIME2 (7)  NULL,
+    [DWCreatedDate]         DATETIME2 (7)  NULL,
+    [DWModifiedDate]        DATETIME2 (7)  NULL,
+    [DWIsDeletedInSource]   BIT            NULL,
+    [DWDeletedInSourceDate] DATETIME2 (7)  NULL,
+    CONSTRAINT [PK_nrmcusthasproductkeyname_History] PRIMARY KEY NONCLUSTERED ([NUUDL_ID] ASC, [DWValidFromDate] ASC)
+);
+
+
+GO
+CREATE CLUSTERED COLUMNSTORE INDEX [CCI_nrmcusthasproductkeyname_History]
+    ON [sourceNuudlNetCracker].[nrmcusthasproductkeyname_History];
+
