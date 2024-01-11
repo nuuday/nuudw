3e3a41e87794c0de700747359afe89e6937e9166
Mohamed Jalali <moja@nuuday.dk>
2024-01-11 14:37:11 +0100

    c

--- SSMS/SSMS/Security/martView_PRX.sql
+++ SSMS/SSMS/Security/martView_PRX.sql
@@ -4,7 +4,15 @@
 
 
 
+
+
 GO
 GRANT SELECT
     ON SCHEMA::[martView_PRX] TO [nuudw-acl01-sql-db-reader];
+
+
+GO
+GRANT SELECT
+    ON SCHEMA::[martView_PRX] TO [nuudw-acl01-sql-schema-mart-prx-reader]
+    WITH GRANT OPTION;
 
