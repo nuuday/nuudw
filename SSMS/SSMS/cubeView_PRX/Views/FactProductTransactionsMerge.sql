811d1aea94b95a39699ff071b5d158f5df4f2e9d
Mohamed Jalali <moja@nuuday.dk>
2024-02-19 14:37:31 +0100

    c

--- SSMS/SSMS/cubeView_PRX/Views/FactProductTransactionsMerge.sql
+++
@@ -1,5 +0,0 @@
-﻿
-CREATE VIEW [cubeView_PRX].[FactProductTransactionsMerge]
-AS
-SELECT 	[BillingAccountID],	[SubscriptionID],	[CalendarID],	[TimeID],	[ProductID],	[CustomerID],	[AddressBillingID],	[HouseHoldID],	[SalesChannelID],	[TransactionStateID],	[ProductTransactionsQuantity],	[ProductChurnQuantity],	[CalendarToID],	[TimeToID],	[CalendarCommitmentToID],	[TimeCommitmentToID],	[PhoneDetailID],	[TLO],	[ProductParentID],	[SubscriptionParentID],	[RGU],	[CalendarRGUID],	[CalendarRGUToID],	[Migration],	[ProductUpgrade]
-FROM [factView].[ProductTransactionsMerge]
