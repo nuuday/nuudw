c54b6399ab0629e22923892aa1f430fde9fe42a5
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-04-05 09:33:24 +0200

    Updated dimension

--- SSMS/SSMS/dim/Tables/Time.sql
+++ SSMS/SSMS/dim/Tables/Time.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE [dim].[Time] (
-    [TimeID]       INT          IDENTITY (1, 1) NOT NULL,
-    [TimeKey]      TIME (0)     NULL,
-    [TimeNotation] NVARCHAR (8) NULL,
-    [TimeHour]     NVARCHAR (2) NULL,
-    [TimeMinute]   NVARCHAR (2) NULL
+    [TimeID]  INT      IDENTITY (1, 1) NOT NULL,
+    [TimeKey] TIME (0) NULL
 );
+
+
 
 
 
--- SSMS/SSMS/martView_PRX/Views/DimTime.sql
+++ SSMS/SSMS/martView_PRX/Views/DimTime.sql
@@ -1,9 +1,7 @@
-﻿CREATE VIEW [martView_PRX].[DimTime]
+﻿
+CREATE VIEW [martView_PRX].[DimTime]
 AS
 SELECT 
 	[TimeID]
 	,[TimeKey]
-	,[TimeNotation] 
-	,TimeHour
-	,TimeMinute
 FROM [dimView].[Time]
