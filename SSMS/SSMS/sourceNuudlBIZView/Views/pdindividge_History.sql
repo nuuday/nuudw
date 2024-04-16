a4100196ae10d7c3dad1364273007e152102ac78
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-04-16 18:06:51 +0200

    Updated BIZ tables

---
+++ SSMS/SSMS/sourceNuudlBIZView/Views/pdindividge_History.sql
@@ -0,0 +1,78 @@
+﻿
+CREATE VIEW [sourceNuudlBIZView].[pdindividge_History]
+AS
+SELECT 
+	[NUUDL_SourceCreated] ,
+	[NUUDL_SourceUpdated] ,
+	[FIRMA] ,
+	[ENHED] ,
+	[VIRKNR] ,
+	[LOENNR] ,
+	[LOEBENR] ,
+	[STATUS] ,
+	[ANUMMER] ,
+	[EMAIL] ,
+	[STEDNR] ,
+	[AONR] ,
+	[KONTORFORK] ,
+	[KONTORNAVN] ,
+	[FYSADR] ,
+	[POSTADR] ,
+	[STIL_KODE] ,
+	[INITIALER] ,
+	[NAVN] ,
+	[FORNAVN] ,
+	[MELLEMNAVN] ,
+	[EFTERNAVN] ,
+	[ADRESSE] ,
+	[ADRLIN_1] ,
+	[ADRLIN_2] ,
+	[ADRLIN_3] ,
+	[POSTNR] ,
+	[KOEN] ,
+	[ANS_DATO] ,
+	[AFG_DATO] ,
+	[FOD_DATO] ,
+	[START_ORLOV] ,
+	[SLUT_ORLOV] ,
+	[OMRAADE] ,
+	[TLFLOK] ,
+	[TLFDIR] ,
+	[TLFMOB] ,
+	[TLFALT] ,
+	[TLFFAX] ,
+	[TLFOPS] ,
+	[TLFUDL] ,
+	[TJEGREN] ,
+	[KATEGORI] ,
+	[AFLFORM] ,
+	[TILHOER] ,
+	[ARBSTEDKD] ,
+	[AENDRET] ,
+	[OPDATERET] ,
+	[NORMTID] ,
+	[TRGRUPPE] ,
+	[BANKREGNR] ,
+	[BANKKONTONR] ,
+	[FAGGRUPPE] ,
+	[NUUDL_ID] ,
+	[NUUDL_ValidFrom] ,
+	[NUUDL_ValidTo] ,
+	[NUUDL_IsCurrent] ,
+	[BIZ_BatchCreatedID] ,
+	[BIZ_BatchUpdatedID] ,
+	[NUUDL_PKLatest] ,
+	[NUUDL_BaseSourceFilename] ,
+	[NUUDL_BaseBatchID] ,
+	[NUUDL_BaseProcessedTimestamp] ,
+	[NUUDL_CuratedBatchID] ,
+	[NUUDL_CuratedProcessedTimestamp] 
+	,[DWIsCurrent]
+	,[DWValidFromDate]
+	,[DWValidToDate]
+	,[DWCreatedDate]
+	,[DWModifiedDate]
+	,[DWIsDeletedInSource]
+	,[DWDeletedInSourceDate]
+FROM [sourceNuudlBIZ].[pdindividge_History]
+WHERE DWIsCurrent = 1
