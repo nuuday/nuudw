6e9855f16cb22dbe23492e7a7e8e8e011daac6db
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-04-16 18:36:10 +0200

    Added SQL objects

---
+++ SSMS/SSMS/dimView/Views/Employee.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dimView].[Employee] 
+AS
+SELECT
+	[EmployeeID]
+	,[EmployeeKey] AS [EmployeeKey]
+	,[EmployeeName] AS [EmployeeName]
+	,[EmployeeEmail] AS [EmployeeEmail]
+	,[OrganizationalLevel1] AS [OrganizationalLevel1]
+	,[OrganizationalLevel2] AS [OrganizationalLevel2]
+	,[OrganizationalLevel3] AS [OrganizationalLevel3]
+	,[DWIsCurrent]
+	,[DWValidFromDate]
+	,[DWValidToDate]
+	,[DWCreatedDate]
+	,[DWModifiedDate]
+	,[DWIsDeleted]
+	
+FROM [dim].[Employee]
---
+++ SSMS/SSMS/stage/Tables/Dim_Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [stage].[Dim_Employee] (
+    [EmployeeKey]          NVARCHAR (6)  NULL,
+    [EmployeeName]         NVARCHAR (50) NULL,
+    [EmployeeEmail]        NVARCHAR (50) NULL,
+    [OrganizationalLevel1] NVARCHAR (1)  NULL,
+    [OrganizationalLevel2] NVARCHAR (2)  NULL,
+    [OrganizationalLevel3] NVARCHAR (3)  NULL,
+    [DWCreatedDate]        DATETIME2 (0) DEFAULT (sysdatetime()) NULL
+);
+
