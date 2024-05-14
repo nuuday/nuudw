5064e251161303923c802bf03d2ef22fd749d748
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-05-14 13:46:06 +0200

    Add ticket views

---
+++ SSMS/SSMS/cubeView_PRX/Views/Ticket.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW cubeView_PRX.[Ticket] 
+AS
+SELECT
+	[TicketID]
+	,[TicketKey] AS [TicketKey]
+	,[TicketCategory] AS [TicketCategory]
+	,[TicketType] AS [TicketType]
+	,[TicketStatus] AS [TicketStatus]
+	,[DWIsCurrent]
+	,[DWValidFromDate]
+	,[DWValidToDate]
+	,[DWCreatedDate]
+	,[DWModifiedDate]
+	,[DWIsDeleted]	
+FROM [dimView].[Ticket]
---
+++ SSMS/SSMS/martView_PRX/Views/Ticket.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW martView_PRX.[Ticket] 
+AS
+SELECT
+	[TicketID]
+	,[TicketKey] AS [TicketKey]
+	,[TicketCategory] AS [TicketCategory]
+	,[TicketType] AS [TicketType]
+	,[TicketStatus] AS [TicketStatus]
+	,[DWIsCurrent]
+	,[DWValidFromDate]
+	,[DWValidToDate]
+	,[DWCreatedDate]
+	,[DWModifiedDate]
+	,[DWIsDeleted]	
+FROM [dimView].[Ticket]
