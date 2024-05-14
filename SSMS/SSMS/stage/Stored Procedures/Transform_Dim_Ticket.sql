ed54b9b45a953efea13ff2a505c15be9298aeedb
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-05-14 13:40:45 +0200

    Add Ticket sql objects

---
+++ SSMS/SSMS/stage/Stored Procedures/Transform_Dim_Ticket.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE PROCEDURE [stage].[Transform_Dim_Ticket]
+	@JobIsIncremental BIT			
+AS 
+
+TRUNCATE TABLE [stage].[Dim_Ticket]
+
+INSERT INTO stage.[Dim_Ticket] WITH (TABLOCK) (TicketKey, TicketCategory, TicketType, TicketStatus)
+SELECT 
+	id AS TicketKey
+	, ticket_category AS TicketCategory
+	, ticket_type AS TicketType
+	, [status] AS TicketStatus
+FROM [sourceNuudlNetCrackerView].[cpmnrmltroubleticket_History]
---
+++ SSMS/SSMS/stage/Tables/Dim_Ticket.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [stage].[Dim_Ticket] (
+    [TicketKey]      NVARCHAR (36)  NULL,
+    [TicketCategory] NVARCHAR (100) NULL,
+    [TicketType]     NVARCHAR (50)  NULL,
+    [TicketStatus]   NVARCHAR (50)  NULL,
+    [DWCreatedDate]  DATETIME2 (0)  DEFAULT (sysdatetime()) NULL
+);
+
