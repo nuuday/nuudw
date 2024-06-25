28d228d77c977e1e08f3d36db2454fef2d76c5b4
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-06-25 14:19:54 +0200

    Added new objects

---
+++ SSMS/SSMS/sourceNuudlNetCracker/Stored Procedures/Initiate_Filter_ibsitemshistory_History.sql
@@ -0,0 +1,40 @@
+﻿
+CREATE PROCEDURE [sourceNuudlNetCracker].Initiate_Filter_ibsitemshistory_History
+AS
+
+DROP TABLE IF EXISTS #ids
+CREATE TABLE #ids (
+	id nvarchar(36)
+)
+
+
+INSERT INTO #ids (id)
+SELECT DISTINCT a.id
+FROM [sourceNuudlNetCracker].[ibsitemshistory_History] a
+INNER JOIN [sourceNuudlNetCracker].[pimnrmldistributionchannel_History] b
+	ON b.id = a.item_json_distributionChannelId 
+WHERE b.name IN ('Irma Showcase','Irma Shadow','YouSee Irma Showcase','YouSee Irma Shadow')
+	AND state = 'PLANNED'
+
+
+INSERT INTO #ids (id)
+SELECT DISTINCT a.id
+FROM [sourceNuudlNetCracker].[ibsitemshistory_History] a
+INNER JOIN [sourceNuudlNetCracker].[pimnrmldistributionchannel_History] b
+	ON b.id = a.item_json_distributionChannelId 
+WHERE 
+	EXISTS (SELECT * FROM #ids WHERE id = a.item_json_parentId)
+	AND NOT EXISTS (SELECT * FROM #ids WHERE id = a.id)
+
+INSERT INTO #ids (id)
+SELECT DISTINCT a.id
+FROM [sourceNuudlNetCracker].[ibsitemshistory_History] a
+LEFT JOIN [sourceNuudlNetCrackerView].[pimnrmlproductoffering_History] b
+	ON b.id = a.item_json_offeringId
+WHERE 
+	a.active_from < '2024-02-06'
+	AND extended_parameters_json_deviceType IS NULL
+
+TRUNCATE TABLE [sourceNuudlNetCracker].[ibsitemshistory_History_Filter]
+INSERT INTO [sourceNuudlNetCracker].[ibsitemshistory_History_Filter] (id)
+SELECT DISTINCT id FROM #ids
