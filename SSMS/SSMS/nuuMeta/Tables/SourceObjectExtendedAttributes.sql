e4817028f85e781054539b870ee54db08b1d6fb8
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-07-12 10:12:22 +0200

    u

---
+++ SSMS/SSMS/nuuMeta/Tables/SourceObjectExtendedAttributes.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [nuuMeta].[SourceObjectExtendedAttributes] (
+    [ID]                    INT                                                IDENTITY (1, 1) NOT NULL,
+    [SourceObjectID]        INT                                                NULL,
+    [SourceColumn]          [sysname]                                          NOT NULL,
+    [SourceColumnAttribute] [sysname]                                          NOT NULL,
+    [DestinationColumn]     [sysname]                                          NOT NULL,
+    [ValidFrom]             DATETIME2 (7) GENERATED ALWAYS AS ROW START HIDDEN CONSTRAINT [DF_SourceObjectExtendedAttributes_ValidFrom] DEFAULT (sysutcdatetime()) NOT NULL,
+    [ValidTo]               DATETIME2 (7) GENERATED ALWAYS AS ROW END HIDDEN   CONSTRAINT [DF_SourceObjectExtendedAttributes_ValidTo] DEFAULT (CONVERT([datetime2],'9999-12-31 23:59:59.9999999')) NOT NULL,
+    CONSTRAINT [PK_SourceObjectExtendedAttributes] PRIMARY KEY CLUSTERED ([ID] ASC),
+    PERIOD FOR SYSTEM_TIME ([ValidFrom], [ValidTo])
+)
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[nuuMeta].[SourceObjectExtendedAttributes_History], DATA_CONSISTENCY_CHECK=ON));
+
---
+++ SSMS/SSMS/nuuMeta/Tables/SourceObjectExtendedAttributes_History.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [nuuMeta].[SourceObjectExtendedAttributes_History] (
+    [ID]                    INT           NOT NULL,
+    [SourceObjectID]        INT           NULL,
+    [SourceColumn]          [sysname]     NOT NULL,
+    [SourceColumnAttribute] [sysname]     NOT NULL,
+    [DestinationColumn]     [sysname]     NOT NULL,
+    [ValidFrom]             DATETIME2 (7) NOT NULL,
+    [ValidTo]               DATETIME2 (7) NOT NULL
+);
+
+
+GO
+CREATE CLUSTERED INDEX [ix_SourceObjectExtendedAttributes_History]
+    ON [nuuMeta].[SourceObjectExtendedAttributes_History]([ValidTo] ASC, [ValidFrom] ASC) WITH (DATA_COMPRESSION = PAGE);
+
