4fa6a4946f4d1d660079e4204dbac977808cd30e
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-07-31 09:20:23 +0200

    added view

---
+++ SSMS/SSMS/martView_PRX/Views/FactProductPrices.sql
@@ -0,0 +1,12 @@
+﻿
+
+CREATE VIEW martView_PRX.[FactProductPrices] 
+AS
+SELECT
+	[CalendarFromID]
+	,[CalendarToID]
+	,[ProductID]
+	,[ActivationBasePriceInclTax]
+	,[DeactivationBasePriceInclTax]
+	,[MonthlyBasePriceInclTax]	
+FROM [factView].[ProductPrices]
