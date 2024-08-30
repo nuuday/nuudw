829b7ecfe6b71abdd5de7a2ac7cbece09a8ee185
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-08-30 14:20:30 +0200

    Added ProductMigrations and fixe ProductPrices

---
+++ SSMS/SSMS/troubleshooting/Views/stage_Fact_OrderEvents.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW troubleshooting.stage_Fact_OrderEvents
+AS
+
+SELECT oe.OrderEventName, pr.ProductName, pr.ProductType, f.*
+FROM stage.Fact_OrderEvents f
+LEFT JOIN dim.OrderEvent oe ON oe.OrderEventKey = f.OrderEventKey
+LEFT JOIN dim.Product pr ON pr.ProductKey = f.ProductKey
