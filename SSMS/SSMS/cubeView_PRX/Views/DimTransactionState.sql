25ea99baab90bcf37ec2f769fc36440c60dbe772
Mikkel Langelykke <131775383+MikkelLangelykke@users.noreply.github.com>
2024-08-30 13:38:43 +0200

    Removed ProductTransactions and TransactionState

--- SSMS/SSMS/cubeView_PRX/Views/DimTransactionState.sql
+++
@@ -1,5 +0,0 @@
-﻿
-CREATE VIEW [cubeView_PRX].[DimTransactionState]
-AS
-SELECT 	[TransactionStateID],	[TransactionStateKey],	[TransactionStateName]
-FROM [dimView].[TransactionState]
--- SSMS/SSMS/stage/Tables/Dim_TransactionState.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [stage].[Dim_TransactionState] (
-    [TransactionStateKey]  NVARCHAR (1)  NULL,
-    [TransactionStateName] NVARCHAR (20) NULL,
-    [DWCreatedDate]        DATETIME      NOT NULL
-);
-
