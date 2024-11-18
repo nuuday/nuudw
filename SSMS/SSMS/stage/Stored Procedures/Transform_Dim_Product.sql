0c53ed40bad541775e9ccbb379c0f97df3343be7
pchu <pchu@nuuday.dk>
2024-11-18 13:08:19 +0100

    czhang weight to NVARCHAR(30) in dimProduct

--- SSMS/SSMS/stage/Stored Procedures/Transform_Dim_Product.sql
+++ SSMS/SSMS/stage/Stored Procedures/Transform_Dim_Product.sql
@@ -10,7 +10,7 @@ SELECT
 	CONVERT( NVARCHAR(36), p.[id] ) AS ProductKey,
 	CONVERT( NVARCHAR(250), ISNULL( NULLIF( p.[name], '' ), '?' ) ) AS ProductName,
 	CONVERT( NVARCHAR(50), ISNULL( NULLIF( pf.[name], '' ), '?' ) ) AS ProductType,  
-	CONVERT( INT, p.weight)  AS ProductWeight
+	CONVERT( NVARCHAR(30), p.weight)  AS ProductWeight
 FROM [sourceNuudlNetCrackerView].[pimnrmlproductoffering_History] p
 LEFT JOIN [sourceNuudlNetCrackerView].[pimnrmlproductfamily_History] pf
 	ON p.product_family_id = pf.id
