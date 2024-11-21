65c9abac6a5783f9ff75a43d5d66e229eb55e458
Adam Rubczyński <adru@nuuday.dk>
2024-11-21 15:18:46 +0100

    Data logging objects for PE

---
+++ SSMS/SSMS/nuuMeta/Tables/PEDataLoadLog.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [nuuMeta].[PEDataLoadLog] (
+    [id]          INT             IDENTITY (1, 1) NOT NULL,
+    [LastLoad]    DATETIME        NULL,
+    [NoOfRecords] INT             NULL,
+    [TableName]   NVARCHAR (400)  NULL,
+    [Status]      NVARCHAR (1000) NULL
+);
+
